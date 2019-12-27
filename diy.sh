9b66345e5a1ba5670b75043d2900abac898db4d3
simm6688 <51709521+simm6688@users.noreply.github.com>
2019-12-27 19:15:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,4 @@ echo '修改机器名称'
 sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 echo '修改wifi名称'
 sed -i 's/OpenWrt/westlake5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/4.14/4.19/g' /target/linux/ipq40xx/Makefile
